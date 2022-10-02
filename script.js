169f5b1376e01a7265eb6f2e20e7e0b46ae99658
jesusbriones <leojesusbriones2@gmail.com>
2022-10-02 14:10:53 -0500

    update image

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-let counter = 0, interval = null;
+(() => {
+	
+	let counter = 0;
+	const container = document.querySelector('.container');
 
-document.getElementById('container-counter').
-addEventListener( 'click', (e) => {
-	if ( e.target.id == 'arrow_left' ) counter--;
-	if ( e.target.id == 'arrow_right' ) counter++;
-	if ( e.target.id == 'counter_num' ) {
-		counter = 0;
-		if ( interval == null ) {
-			document.getElementById('button__counter').textContent = 'Iniciar';
-		}
-	}
-	document.getElementById('counter_num').textContent = counter;
-});
+	container.addEventListener('click', (e) => {
+		counter++;
+		document.querySelector('.counter').textContent = counter;
+		const circle = document.createElement('DIV');
+		circle.classList.add('circle');
+		circle.style.top = `${e.clientY-50}px`;
+		circle.style.left = `${e.clientX-50}px`;
+		container.appendChild(circle);
+		
+		setTimeout(() => {container.removeChild(circle);}, 1000)
+	});
 
 
-document.getElementById('button__counter')
-.addEventListener( 'click', (e) => {
-
-	if ( interval == null ) {
-		e.target.textContent = 'Detener';
-		interval = setInterval( () => {
-			counter++;
-			document.getElementById('counter_num').textContent = counter;
-		}, 1000 );
-	}else {
-		e.target.textContent = 'Reanudar';
-		clearInterval( interval );
-		interval = null;
-	}
-});
+})();
